1d9b93c28bb1a0968789f25fd5ede0631797722c
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-19 18:26:14 -0500

    feat: Add register() and getAddress() functions to contract

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+  // mapping data type to store names
+  mapping(string => address) public domains;
+
   constructor() {
     console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+  }
+
+  // A register function that adds names to our mapping
+  function register(string calldata name) public {
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+
+  // This will give us the domain owners' address
+  function getAddress(string calldata name) public view returns (address) {
+    return domains[name];
   }
 }
