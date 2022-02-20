f6f70bd5b40d271ebc8d08f22f6f5c1d722b218d
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-19 19:07:24 -0500

    feat: Add domain TLD and set price based on domain length

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,23 +2,45 @@
 
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+  // domain TLD
+  string public tld;
+
   // mapping data type to store names
   mapping(string => address) public domains;
-
   // mapping to store values
   mapping(string => string) public records;
 
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT.");
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
+  }
+
+  // price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17; // 0.5 MATIC = 500 000 000 000 000 000 (17 decimals).
+    } else if (len == 4) {
+      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3 MATIC
+    } else {
+      return 1 * 10**17;
+    }
   }
 
   // A register function that adds names to our mapping
-  function register(string calldata name) public {
+  function register(string calldata name) public payable {
     // Check that the name is unregistered
     require(domains[name] == address(0));
+
+    uint _price = price(name);
+
+    // Check if enough Matic was paid in the transaction
+    require(msg.value >= _price, "Not enough Matic paid");
 
     domains[name] = msg.sender;
     console.log("%s has registered a domain!", msg.sender);
--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,20 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  const domainContract = await domainContractFactory.deploy();
+  // pass in "pshhh" to the constructor when deploying
+  const domainContract = await domainContractFactory.deploy("pshhh");
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
 
-  const txn = await domainContract.register("doom");
+  // pass in a second variable - value
+  let txn = await domainContract.register("whydontyou", { value: hre.ethers.utils.parseEther('0.1') });
   await txn.wait();
 
-  const domainOwner = await domainContract.getAddress("doom");
-  console.log("Owner of domain:", domainOwner);
+  const address = await domainContract.getAddress("whydontyou");
+  console.log("Owner of domain whydontyou:", address);
 
-  // Trying to set a record that doesn't belong to me!
-  txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
-  await txn.wait();
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
 
 const runMain = async () => {
