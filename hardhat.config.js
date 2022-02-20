19a169a80a68233fae68179c53a134c1f9efc9bf
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-19 20:09:11 -0500

    feat: Add script to deploy contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,5 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +9,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+		mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_ENDPOINT,
+      accounts: [process.env.MUMBAI_PRIVATE_KEY],
+		}
+  }
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,33 @@
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const domainContract = await domainContractFactory.deploy("ninja");
+  await domainContract.deployed();
+
+  console.log("Contract deployed to:", domainContract.address);
+
+	let txn = await domainContract.register("holymoley", { value: hre.ethers.utils.parseEther('0.1') });
+	await txn.wait();
+  console.log("Minted domain holymoley.pshhh");
+
+  txn = await domainContract.setRecord("holymoley", "Are you holymoley??");
+  await txn.wait();
+  console.log("Set record for holymoley.pshhh");
+
+  const address = await domainContract.getAddress("holymoley");
+  console.log("Owner of domain holymoley:", address);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
